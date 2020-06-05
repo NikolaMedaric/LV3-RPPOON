da1fd0b7e68e14ab01840b653f6af8af9210a90e
NikolaMedaric <63855981+NikolaMedaric@users.noreply.github.com>
2020-06-05 18:05:18 +0200

    Add files via upload

---
+++ MatrixGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV
+{
+    class MatrixGenerator
+    {
+        private static MatrixGenerator instance;       
+        private Random generator; 
+ 
+        private MatrixGenerator()         
+        {         
+            this.generator = new Random();  
+        } 
+ 
+        public static MatrixGenerator GetInstance()     
+        {          
+            if (instance == null)          
+            {       
+                instance = new MatrixGenerator();   
+            }      
+            return instance; 
+        }  
+
+        public double NextDouble()         
+        {           
+            return this.generator.NextDouble();   
+        }
+        public double[][] CreateMatrix(int rows, int columns)
+        {
+            double[,] newMatrix = new double[rows,columns];
+            for (int i = 0; i < rows; i++)
+            {
+                for(int j=0;j < columns; j++)
+                {
+                newMatrix[i,j] = NextDouble();
+                }
+            }
+            return newMatrix[rows,columns];
+        }
+    }
+}
